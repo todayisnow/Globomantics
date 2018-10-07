673afd5e14fc335936a006cac9379ccec03080e0
Essam <eaboelsaoud@mofa.gov.sa>
2018-10-07 07:12:52 +0300

    changes

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -1,34 +1,74 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Globalization;
+using System.Net.Http;
+using WebApp.Services;
+
 
 namespace WebApp
 {
     public class Startup
     {
+        private IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddSingleton<IConferenceService, ConferenceMemoryService>();
+            services.AddSingleton<IProposalService, ProposalMemoryService>();
+            services.AddSingleton(x =>
+                new HttpClient { BaseAddress = new Uri("http://localhost:5000") });
+
+            services.Configure<GlobomanticsOptions>(_configuration.GetSection("Globomantics"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILogger<Startup> log)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseStaticFiles();
+            log.LogError("ddddd");
+            app.Use(next =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                return async context =>
+                {
+                    log.LogInformation("Incomming Request");
+                    if (context.Request.Path.StartsWithSegments("/mym"))
+                    {
+                        await context.Response.WriteAsync("Hit Custom Middleware");
+                        log.LogInformation("Handled Request");
+
+                    }
+                    else
+                    {
+                        await next(context);
+                        log.LogInformation("outgoing Response");
+
+                    }
+                };
             });
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Conference}/{action=Index}/{id?}");
+            });
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
         }
     }
 }
